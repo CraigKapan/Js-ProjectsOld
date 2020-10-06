43168b8ab3987bc163e6b3e6e05c3ea6d2f9d6d6
Widikin <widikin@Julies-MacBook-Pro.local>
2020-10-06 14:15:32 +0200

    finished object oriented programming

--- Object-oriented Programming/script.js
+++ Object-oriented Programming/script.js
@@ -1,2 +1,29 @@
 
-new String(); // '', "", ``
+function Circle(radius) {
+    this.radius = radius;
+
+    let defaultLocation = { x: 0, y: 0};
+
+    this.getDefaultLocation = function() {
+        return defaultLocation;
+    };
+
+    this.draw = function() {
+        console.log('draw');
+    };
+
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function() {
+            return defaultLocation;
+        },
+        set: function(value) {
+            if (!value.x || !value.y) 
+                throw new Error('Invalid location.');
+
+            defaultLocation = value;
+        }
+    });
+};
+
+const circle = new Circle(10);
+circle.draw();
